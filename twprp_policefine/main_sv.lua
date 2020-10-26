5a8671ec736d6995dbded54022c765b12a4a2f58
Roids-Dev <54599495+itsroids@users.noreply.github.com>
2020-10-26 02:45:05 -0500

    Pushing fixes made by Neo Green #7240 :smile:

--- twprp_policefine/main_sv.lua
+++ twprp_policefine/main_sv.lua
@@ -19,22 +19,24 @@ AddEventHandler("twprp:fine", function(criminal, fine)
 	local _fine = fine
 	--local User = VorpCore.getUser(_criminal)
 	--local Character = User.getUsedCharacter
-	TriggerEvent("vorp:getCharacter",_criminal,function(user)
+	
 
 	local Character = VorpCore.getUser(_source).getUsedCharacter
+	local Receiver = VorpCore.getUser(_criminal).getUsedCharacter
     local job = Character.job
     if job == 'police' then
-			print('cop working')
-			TriggerClientEvent("vorp:Tip", _criminal, 'You have been fined $'.._fine, 5000)
-			VORP.removeMoney(_criminal, 0, _fine)
-			Citizen.Wait(5500)
-			TriggerClientEvent("vorp:Tip", _source, user.firstname..' '..user.lastname..' has been fined $'.._fine, 5000)
-		end
-		if job ~= 'police' then
-			TriggerClientEvent("vorp:Tip", _source, "Only lawmen can do that. You are not a lawman!", 5000)
-				print('Not a cop working')
-		end
-	end)
+			--print('cop working')
+		TriggerClientEvent("vorp:TipBottom", _criminal, 'You have been Fined $'.._fine, 5000)
+		TriggerClientEvent("vorp:TipBottom", _source , Receiver.firstname..' '..Receiver.lastname..' has been Fined $'.._fine, 5000)
+		--TriggerClientEvent("vorp:TipBottom", _source, 'You have Fined $'.._fine, 5000)
+		VORP.removeMoney(_criminal, 0, _fine)
+		Citizen.Wait(2000)
+	end
+	if job ~= 'police' then
+		TriggerClientEvent("vorp:TipBottom", _source, "Only lawmen can do that. You are not a lawman!", 5000)
+			--print('Not a cop working')
+	end
+	
 end)
 
 
