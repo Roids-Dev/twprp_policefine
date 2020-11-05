fe679d2e1629ddcde87360dcb99b1de6a7b789a2
Roids-Dev <73410725+Roids-Dev@users.noreply.github.com>
2020-11-05 11:37:02 -0600

    added config.lua

--- twprp_policefine/fxmanifest.lua
+++ twprp_policefine/fxmanifest.lua
@@ -5,10 +5,11 @@ games {"rdr3"}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 client_script {
-	'main_cl.lua'
+	'main_cl.lua',
+	'Config.lua'
 				}
 
 server_scripts {
     'main_sv.lua',
-    '@mysql-async/lib/MySQL.lua'
+    'Config.lua',
 }
